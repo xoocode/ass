c3cc25b49bbbdd0953bb7eadfbb4df70606ab953
xoocode <peter@xoocode.com>
2023-05-21 11:57:40 +0200

    Prepped npm package

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,31 @@
-declare function slugify(string: string): string
-declare function unslugify(slug: string): string
-declare function sanitize(str: string): string
-declare function countSpecialChars(str: string): number
-declare function filterItems(contentItems: string[], oldItems: string[], blacklist?: RegExp[]): string[]
-declare function getItemsFromContent(content: string): string[]
-declare function parseObjectsFromText(content: string, properties: string[]): any[]
-declare function seededRandom(seed: number): number
-declare function pickOne(arr: any[], h?: number): any
-declare function pickMany(arr: any[], limit?: number, h?: number): any[]
-declare function removeDuplicatesFromArray<T>(array: T[], compare?: keyof T | null): T[]
-declare function decodeHtmlEntities(text: string): string
-declare function escapeRegExp(string: string): string
-declare function log(...args: unknown[]): void
-declare function info(...args: unknown[]): void
-declare function warn(...args: unknown[]): void
-declare function error(...args: unknown[]): void
-declare function post(...args: unknown[]): void
-declare function get(...args: unknown[]): void
-declare function throwApiError(error: any, message?: string): string
-declare function toTitleCase(str: string): string
-declare function toUpperCase(str: string): string
-declare function toLowerCase(str: string): string
-declare function toCamelCase(str: string): string
-declare function toPascalCase(str: string): string
-declare function toHyphenCase(str: string): string
-declare function toSnakeCase(str: string): string
-declare function toDotCase(str: string): string
-declare function toSentenceCase(str: string): string
-declare function toUpperHyphenCase(str: string): string
-declare function getRandomLoremIpsum({ minLength, maxLength, textCase }?: { minLength?: number; maxLength?: number; textCase?: string }): string
+export declare function slugify(string: string): string
+export declare function unslugify(slug: string): string
+export declare function sanitize(str: string): string
+export declare function countSpecialChars(str: string): number
+export declare function filterItems(contentItems: string[], oldItems: string[], blacklist?: RegExp[]): string[]
+export declare function getItemsFromContent(content: string): string[]
+export declare function parseObjectsFromText(content: string, properties: string[]): any[]
+export declare function seededRandom(seed: number): number
+export declare function pickOne(arr: any[], h?: number): any
+export declare function pickMany(arr: any[], limit?: number, h?: number): any[]
+export declare function removeDuplicatesFromArray<T>(array: T[], compare?: keyof T | null): T[]
+export declare function decodeHtmlEntities(text: string): string
+export declare function escapeRegExp(string: string): string
+export declare function log(...args: unknown[]): void
+export declare function info(...args: unknown[]): void
+export declare function warn(...args: unknown[]): void
+export declare function error(...args: unknown[]): void
+export declare function post(...args: unknown[]): void
+export declare function get(...args: unknown[]): void
+export declare function throwApiError(error: any, message?: string): string
+export declare function toTitleCase(str: string): string
+export declare function toUpperCase(str: string): string
+export declare function toLowerCase(str: string): string
+export declare function toCamelCase(str: string): string
+export declare function toPascalCase(str: string): string
+export declare function toHyphenCase(str: string): string
+export declare function toSnakeCase(str: string): string
+export declare function toDotCase(str: string): string
+export declare function toSentenceCase(str: string): string
+export declare function toUpperHyphenCase(str: string): string
+export declare function getRandomLoremIpsum({ minLength, maxLength, textCase }?: { minLength?: number; maxLength?: number; textCase?: string }): string
